ff85317aac5d68a114a0c47560831a6c1c264558
Jay Issuree <Jay_Issuree@hotmail.co.uk>
2019-11-25 17:10:55 +0000

    victim 1 map added

--- Jack the Ripper/Jack the Ripper/ViewController.swift
+++ Jack the Ripper/Jack the Ripper/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 Team-6. All rights reserved.
 //
 
+import MapKit
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,8 +14,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let location = CLLocationCoordinate2D(latitude: 51.520045,
+            longitude: -0.060630)
+        
+        // 2
+        let span = MKCoordinateSpan(latitudeDelta: 0.04, longitudeDelta: 0.04)
+        let region = MKCoordinateRegion(center: location, span: span)
+            mapView.setRegion(region, animated: true)
+            
+        //3
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Victim 1"
+        annotation.subtitle = "Whitechapel"
+        mapView.addAnnotation(annotation)
+        
     }
 
-
+    @IBOutlet weak var mapView: MKMapView!
+    
 }
-
