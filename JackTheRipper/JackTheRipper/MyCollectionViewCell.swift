c15f60063bbbb9ed4f66d12639ea9b744dad968a
robertamangiapane <roberta.mangiapane@gmail.com>
2019-12-02 13:27:39 +0000

    add challenge3 as wordsearch puzzle

---
+++ JackTheRipper/JackTheRipper/MyCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MyCollectionViewCell.swift
+//  JackTheRipper
+//
+//  Created by Student on 02/12/2019.
+//  Copyright © 2019 Team6. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    static let cellId = "cell"
+    
+    @IBOutlet weak var cellLabel: UILabel!
+    
+    }
