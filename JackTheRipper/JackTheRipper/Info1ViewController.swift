3c79c2bde972c17294f57b1e5a9af395c6fd8ec5
DanielleInkster <danielle.inkster@gmail.com>
2019-12-04 17:33:30 +0000

    test view controller

---
+++ JackTheRipper/JackTheRipper/Info1ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  Info1ViewController.swift
+//  JackTheRipper
+//
+//  Created by Danielle Inkster on 2019-12-04.
+//  Copyright © 2019 Team6. All rights reserved.
+//
+import Foundation
+import UIKit
+
+class Info1ViewController: UIViewController {
+    
+   
+    }
